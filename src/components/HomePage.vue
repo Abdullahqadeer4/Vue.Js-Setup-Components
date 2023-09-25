ff5cd8be6aeda14cb8767ae1e0f6bc2fc92465e9
Abdullahqadeer4 <abdullahqadeer420@gmail.com>
2023-09-25 05:16:42 -0700

    Tutorial

---
+++ src/components/HomePage.vue
@@ -0,0 +1,25 @@
+ <template>
+  <h1>Hello from Home Component</h1>
+  <div>
+     {{data}}
+  </div>
+  <h2>  {{msg}}  </h2>
+</template>
+
+<script>
+export default {
+     name: 'HomePage',
+     props:{
+          data:String,
+          msg: String,
+     }
+}
+</script>
+
+<style>
+
+</style>
+<!-- The name of the components should be always multiword, else it will throw errors. 
+I have shown you the use of props as well {data,msg}. Register your componenets in
+app.vue file by importing the component and pass it into components.
+Also you will need to pass call the component in Templates in App.vue. Check out the app.vue file. -->
